41fddf9c8a101bc5513dfd458569fa3dee3c3c7c
Joel-Baptista <joelbaptista@ua.pt>
2022-12-06 16:30:26 +0000

    Some necessery alterations

---
+++ Datasets/Scripts/dataset_mnist.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from vision_config.vision_definitions import ROOT_DIR
+import numpy as np
+import cv2
+
+
+def count_samples(samples: pd.DataFrame, file: str):
+    count = 0
+    label_list = []
+    count_list = []
+
+    for item in samples["label"]:
+        count += 1
+
+        if item in label_list:
+            count_list[label_list.index(item)] += 1
+        else:
+            label_list.append(item)
+            count_list.append(0)
+
+    label_list, count_list = zip(*sorted(zip(label_list, count_list)))
+
+    print(label_list)
+    print(count_list)
+
+    data_frame = {"Label": label_list,
+                  "Size": count_list}
+
+    df = pd.DataFrame(data_frame)
+    df.to_csv(ROOT_DIR + f"/Datasets/MNIST/{file}_sample_size.csv")
+
+
+file_name = "sign_mnist_train"
+
+# Read the csv file
+data = pd.read_csv(ROOT_DIR + f'/Datasets/MNIST/{file_name}.csv')
+
+# Print it out if you want
+
--- hand_classification/network/transfer_learning.py
+++ hand_classification/network/transfer_learning.py
@@ -1,21 +1,80 @@
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import tensorflow as tf
+import pandas as pd
+from vision_config.vision_definitions import ROOT_DIR
+
+
+def dataset():
+
+    data = pd.read_csv(ROOT_DIR + f'/Datasets/MNIST/sign_mnist_train.csv')
+    x_train = []
+    y_train = []
+
+    for i in range(0, len(data["label"])):
+        sample_in_line = np.array(data.iloc[i][1:])
+
+        sample_in_array = sample_in_line.reshape((28, 28))
+
+        res = cv2.resize(sample_in_array.astype(np.uint8), (32, 32), interpolation=cv2.INTER_LINEAR)
+
+        sample = np.zeros((32, 32, 3))
+        sample[:, :, 0] = res
+        sample[:, :, 1] = res
+        sample[:, :, 2] = res
+
+        label = [0] * 24
+
+        if data.iloc[i][0] >= 10:
+            index = data.iloc[i][0] - 1
+        else:
+            index = data.iloc[i][0]
+
+        label[index] = 1
+
+        x_train.append(sample)
+        y_train.append(label)
+
+    return x_train, y_train
 
 
 if __name__ == '__main__':
-    _URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
-    path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=True)
-    PATH = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered')
 
-    train_dir = os.path.join(PATH, 'train')
-    validation_dir = os.path.join(PATH, 'validation')
+    x_t, y_t = dataset()
 
-    BATCH_SIZE = 1000
-    IMG_SIZE = (160, 160)
+    IMG_SHAPE = (32, 32, 3)
 
-    train_dataset = tf.keras.utils.image_dataset_from_directory(train_dir,
-                                                                shuffle=True,
-                                                                batch_size=BATCH_SIZE,
-                                                                image_size=IMG_SIZE)
+    global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+    base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                                   include_top=False,
+                                                   weights='imagenet')
+
+    base_model.trainable = False
+
+    inputs = tf.keras.Input(shape=(32, 32, 3))
+    x = base_model(inputs, training=False)
+    x = global_average_layer(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+    outputs = tf.keras.layers.Dense(24)(x)
+    model = tf.keras.Model(inputs, outputs)
+
+    base_learning_rate = 0.0001
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
+                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
+
+    history = model.fit(x=np.array(x_t),
+                        y=np.array(y_t),
+                        epochs=50,
+                        validation_split=0.2,
+                        shuffle=True,
+                        callbacks=[callback],
+                        batch_size=2000,
+                        verbose=2)
+
